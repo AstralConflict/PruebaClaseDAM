21020df78fefe355950eda8d74784555124693e2
AstralConflict <lol.lol.lol.lol.5.mor@gmail.com>
2025-09-30 10:46:53 +0200

    Prueba commit desde Eclipse IDE

---
+++ PruebaGit2/src/pruebaGit2/Principal.java
@@ -0,0 +1,11 @@
+package pruebaGit2;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("!Subiendo codigo desde Ecplipse IDE a GitHub!");
+		
+	}
+
+}
